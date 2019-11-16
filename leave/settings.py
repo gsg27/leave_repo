dedbef2772992792edbc09186d71616b63712c9a
gsg27 <gauravgahlyan27@gmail.com>
2019-11-16 20:08:40 +0530

    Added files and changes required for deployment to heroku

--- leave/settings.py
+++ leave/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = '/dashboard'
 LOGOUT_REDIRECT_URL = '/'
 
